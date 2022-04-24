4e90fffee172285d01c39e9286f13cb8f3904928
panweiji <a18863302302>
2022-04-24 09:58:38 +0800

    chore: 基于Lerna管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Taro技术栈

---
+++ projects/taro-mini-demo/src/pages/index/index.tsx
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+import {Text, View} from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+    return (
+      <View className='index'>
+        <Text>Hello Taro!</Text>
+      </View>
+    )
+  }
+}
