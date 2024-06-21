1dc8b0d3222cb59ce9fa0a8eae241fce4bf787be
panweiji <406798106@qq.com>
2024-06-21 14:27:08 +0800

    feat: 基于Pnpm、Lerna管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Taro技术栈

---
+++ projects-demo/taro-mini-demo/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    NODE_ENV: '"development"',
+    BASE_URL: '"https://app-api.rzatfc.com"',
+    IS_DEBUG: 'true'
+  },
+  defineConstants: {},
+  mini: {},
+  h5: {}
+}
---
+++ projects-demo/taro-mini-demo/src/pages/index/index.tsx
@@ -0,0 +1,45 @@
+import { Component } from 'react'
+import { Text, View } from '@tarojs/components'
+import './index.scss'
+import { Pipe, Validate } from 'athena-common'
+import { CommonUtils } from 'athena-utils'
+import { Constant } from 'athena-constants'
+import { DemoReactComponent } from 'athena-components'
+import { demoApi } from '../../api/demo/demo'
+
+export default class Index extends Component {
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+    console.log('Taro多包复用示例')
+    console.log(Pipe.hidePart('18863302302', 'phone'))
+    console.log(CommonUtils.randomString(6, 12))
+    console.log(Constant.PHONE_REGEX)
+    console.log('校验结果: ' + Validate.email('123'))
+    demoApi().then(res => {
+
+    })
+
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+    return (
+      <View className="index">
+        <Text className="primary">Hello Taro!</Text>
+        <DemoReactComponent text="Taro自定义组件"></DemoReactComponent>
+        {/*<WebView src='http://www.jxybkj.cn:8010/?leaguerId=5871741f86894f8a9ce11ceed3d43ec1#/home' />*/}
+      </View>
+    )
+  }
+}
