12038cda9c7464c2165959becb737576d12ae393
panweiji <406798106@qq.com>
2024-06-21 17:22:09 +0800

    feat: 基于Pnpm、Lerna管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Taro技术栈

--- projects/pengbo-mini/src/pages/index/index.vue
+++ projects/pengbo-mini/src/pages/index/index.vue
@@ -2,19 +2,19 @@
   <view class="index">
     <text>{{ msg }}</text>
   </view>
-  <VueComponentDemo message="Hello  VueComponentDemo " />
-  <!--  <DemoVueComponent message="Hello  DemoVueComponent " />-->
+  <VueComponentDemo message="Hello  VueComponentDemo" />
+  <DemoVueComponent message="Hello  DemoVueComponent" />
 </template>
 
 <script>
 import { ref } from 'vue'
 import './index.scss'
-import { testHelloMonoRepo } from 'athena-test'
+import { DemoVueComponent, testHelloMonoRepo } from 'athena-test'
 import VueComponentDemo from '@/components/demo/component-demo.vue'
 
 
 export default {
-  components: { VueComponentDemo },
+  components: { VueComponentDemo, DemoVueComponent },
   setup() {
     const msg = ref('Hello world')
     return {
@@ -27,18 +27,23 @@ export default {
   },
 
   onShow() {
-    console.log('Taro多包复用示例')
-    testHelloMonoRepo()
+    this.testMonoRepo()
 
-    /*  demoApi().then(res => {
+  },
 
-     })
-          console.log(CommonUtils.randomString(6, 12))
-       console.log(Pipe.hidePart('18863302302', 'phone'))
-       console.log(Constant.PHONE_REGEX)
-       console.log('校验结果: ' + Validate.email('123'))
-   */
+  methods: {
+    testMonoRepo() {
+      console.log('Taro多包复用示例')
+      testHelloMonoRepo()
+      /*  demoApi().then(res => {
 
+       })
+         console.log(CommonUtils.randomString(6, 12))
+         console.log(Pipe.hidePart('18863302302', 'phone'))
+         console.log(Constant.PHONE_REGEX)
+         console.log('校验结果: ' + Validate.email('123'))
+     */
+    }
   }
 }
 </script>
