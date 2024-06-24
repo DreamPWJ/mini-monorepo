ae58186f6277cc0cab4de7ea667882d26f954cbf
panweiji <406798106@qq.com>
2024-06-24 17:07:26 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

---
+++ projects-demo/taro-mini-nutui-demo/config/index.ts
@@ -0,0 +1,103 @@
+import Components from 'unplugin-vue-components/webpack'
+import NutUIResolver from '@nutui/auto-import-resolver'
+
+const config = {
+  projectName: 'taro-mini-nutui-demo',
+  date: '2024-6-24',
+  designWidth(input) {
+    if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+      return 375
+    }
+    return 750
+  },
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: ['@tarojs/plugin-html'],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {}
+  },
+  framework: 'vue3',
+  compiler: {
+    type: 'webpack5',
+    prebundle: { enable: false }
+  },
+  sass: {
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+  },
+  mini: {
+    webpackChain(chain) {
+      chain.plugin('unplugin-vue-components').use(Components({
+        resolvers: [
+          NutUIResolver({
+            importStyle: 'sass',
+            taro: true
+          })
+        ]
+      }))
+    },
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {
+          // selectorBlackList: ['nut-']
+        }
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024 // 设定转换尺寸上限
+        }
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  },
+  h5: {
+    webpackChain(chain) {
+      chain.plugin('unplugin-vue-components').use(Components({
+        resolvers: [
+          NutUIResolver({
+            importStyle: 'sass',
+            taro: true
+          })
+        ]
+      }))
+    },
+    publicPath: '/',
+    staticDirectory: 'static',
+    esnextModules: ['nutui-taro', 'icons-vue-taro'],
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {}
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  }
+}
+
+module.exports = function(merge) {
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
+  }
+  return merge({}, config, require('./prod'))
+}
---
+++ projects-demo/taro-mini-nutui-demo/src/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="demo">
+    <view>
+      欢迎使用 NutUI 开发 Taro 多端项目
+      <Dongdong />
+    </view>
+    <view>{{ count }}</view>
+    <view>
+      <nut-button type="primary" @click="handleClick">
+        Count++
+      </nut-button>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Dongdong } from '@nutui/icons-vue-taro'
+
+const count = ref(0)
+
+const handleClick = () => {
+  count.value++
+}
+</script>
+
+<style>
+.demo {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
