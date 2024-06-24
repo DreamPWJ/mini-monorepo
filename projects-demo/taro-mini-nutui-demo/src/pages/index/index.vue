b9aada9746fc5641e6e41982436457e417bdddbd
panweiji <406798106@qq.com>
2024-06-24 18:05:22 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro-mini-nutui-demo/src/pages/index/index.vue
+++ projects-demo/taro-mini-nutui-demo/src/pages/index/index.vue
@@ -10,18 +10,21 @@
         Count++
       </nut-button>
     </view>
+    <VueDemoComponent message="Hello MonoRepo VueComponentDemo"></VueDemoComponent>
   </view>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { Dongdong } from '@nutui/icons-vue-taro'
+import { VueDemoComponent } from 'athena-components-vue'
 
 const count = ref(0)
 
 const handleClick = () => {
   count.value++
 }
+
 </script>
 
 <style>
