44680efae20afbb4047245c9fafc29e21e19ff7b
panweiji <406798106@qq.com>
2024-06-24 20:48:35 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects/pengbo-mini/config/index.ts
+++ projects/pengbo-mini/config/index.ts
@@ -1,103 +1,129 @@
+import { defineConfig, type UserConfigExport } from '@tarojs/cli'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+import devConfig from './dev'
+import prodConfig from './prod'
 import Components from 'unplugin-vue-components/webpack'
 import NutUIResolver from '@nutui/auto-import-resolver'
 
-const config = {
-  projectName: 'pengbo-mini',
-  date: '2024-6-24',
-  designWidth(input) {
-    if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
-      return 375
-    }
-    return 750
-  },
-  deviceRatio: {
-    640: 2.34 / 2,
-    750: 1,
-    828: 1.81 / 2,
-    375: 2 / 1
-  },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
-  plugins: ['@tarojs/plugin-html'],
-  defineConstants: {},
-  copy: {
-    patterns: [],
-    options: {}
-  },
-  framework: 'vue3',
-  compiler: {
-    type: 'webpack5',
-    prebundle: { enable: false }
-  },
-  sass: {
-    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
-  },
-  mini: {
-    webpackChain(chain) {
-      chain.plugin('unplugin-vue-components').use(Components({
-        resolvers: [
-          NutUIResolver({
-            importStyle: 'sass',
-            taro: true
-          })
-        ]
-      }))
-    },
-    postcss: {
-      pxtransform: {
-        enable: true,
-        config: {
-          // selectorBlackList: ['nut-']
-        }
-      },
-      url: {
-        enable: true,
-        config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
+// https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
+export default defineConfig(async (merge, { command, mode }) => {
+  const baseConfig: UserConfigExport = {
+    projectName: 'pengbo-mini',
+    date: '2024-6-21',
+    designWidth(input) {
+      if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+        return 375
       }
-    }
-  },
-  h5: {
-    webpackChain(chain) {
-      chain.plugin('unplugin-vue-components').use(Components({
-        resolvers: [
-          NutUIResolver({
-            importStyle: 'sass',
-            taro: true
-          })
-        ]
-      }))
+      return 750
     },
-    publicPath: '/',
-    staticDirectory: 'static',
-    esnextModules: ['nutui-taro', 'icons-vue-taro'],
-    postcss: {
-      autoprefixer: {
-        enable: true,
-        config: {}
+    deviceRatio: {
+      640: 2.34 / 2,
+      750: 1,
+      828: 1.81 / 2,
+      375: 2 / 1
+    },
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    plugins: ['@tarojs/plugin-html'],
+    defineConstants: {},
+    copy: {
+      patterns: [],
+      options: {}
+    },
+    framework: 'vue3',
+    compiler: {
+      type: 'webpack5',
+      prebundle: { enable: false }
+    },
+    sass: {
+      data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+    },
+    cache: {
+      enable: true // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    },
+    mini: {
+      postcss: {
+        pxtransform: {
+          enable: true,
+          config: {}
+        },
+        url: {
+          enable: true,
+          config: {
+            limit: 1024 // 设定转换尺寸上限
+          }
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
       },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+      webpackChain(chain) {
+        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
+        chain.plugin('unplugin-vue-components').use(Components({
+          resolvers: [
+            NutUIResolver({
+              importStyle: 'sass',
+              taro: true
+            })
+          ]
+        }))
+      }
+    },
+    h5: {
+      publicPath: '/',
+      staticDirectory: 'static',
+      esnextModules: ['nutui-taro', 'icons-vue-taro'],
+      output: {
+        filename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].[chunkhash:8].js'
+      },
+      miniCssExtractPluginOption: {
+        ignoreOrder: true,
+        filename: 'css/[name].[hash].css',
+        chunkFilename: 'css/[name].[chunkhash].css'
+      },
+      postcss: {
+        autoprefixer: {
+          enable: true,
+          config: {}
+        },
+        cssModules: {
+          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+          config: {
+            namingPattern: 'module', // 转换模式，取值为 global/module
+            generateScopedName: '[name]__[local]___[hash:base64:5]'
+          }
+        }
+      },
+      webpackChain(chain) {
+        chain.resolve.plugin('tsconfig-paths').use(TsconfigPathsPlugin)
+        chain.plugin('unplugin-vue-components').use(Components({
+          resolvers: [
+            NutUIResolver({
+              importStyle: 'sass',
+              taro: true
+            })
+          ]
+        }))
+      }
+    },
+    rn: {
+      appName: 'taroDemo',
+      postcss: {
+        cssModules: {
+          enable: false // 默认为 false，如需使用 css modules 功能，则设为 true
         }
       }
     }
   }
-}
-
-module.exports = function(merge) {
   if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
+    // 本地开发构建配置（不混淆压缩）
+    return merge({}, baseConfig, devConfig)
   }
-  return merge({}, config, require('./prod'))
-}
+  // 生产构建配置（默认开启压缩混淆等）
+  return merge({}, baseConfig, prodConfig)
+})
