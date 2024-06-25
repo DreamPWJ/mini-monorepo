4cfe2242250aad9f060958ce1073a4d8cde5cded
panweiji <406798106@qq.com>
2024-06-25 09:22:56 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro-mini-vue-demo/src/pages/index/index.vue
+++ projects-demo/taro-mini-vue-demo/src/pages/index/index.vue
@@ -4,12 +4,12 @@
     <ProjectComponentDemo message="Hello Project VueComponentDemo"></ProjectComponentDemo>
     <VueDemoComponent message="Hello MonoRepo VueComponentDemo"></VueDemoComponent>
     <nut-button type="primary">NutUI小程序组件库</nut-button>
-    <nut-skeleton width="250px" height="15px" animated row="3"> </nut-skeleton>
+    <nut-skeleton width="250px" height="15px" animated row="3"></nut-skeleton>
     <!--    <IconFont name="add" color="#fa2c19" size="24"/>-->
   </view>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ref } from 'vue'
 import './index.scss'
 import { testHelloMonoRepo } from 'athena-test'
@@ -19,45 +19,36 @@ import { demoApi } from '@/api/demo/demo'
 import { VueDemoComponent } from 'athena-components-vue'
 import { Constant } from 'athena-constants'
 import { Pipe, Validate } from 'athena-common'
+import { useDidShow, useLoad } from '@tarojs/taro'
 
-export default {
-  components: {
-    ProjectComponentDemo,
-    VueDemoComponent
-  },
+/**
+ * 数据定义
+ */
+const msg = ref('Hello Taro Mini Pnpm MonoRepo')
 
-  setup() {
-    const msg = ref('Hello Taro Mini Pnpm MonoRepo')
-    return {
-      msg
-    }
-  },
+useLoad(() => {
+  console.log('Index onLoad')
+})
 
-  onLoad() {
-    console.log('Index onLoad')
-  },
+useDidShow(() =>
+  testMonoRepo()
+)
 
-  onShow() {
-    this.testMonoRepo()
+/**
+ * 测试MonoRepo
+ */
+const testMonoRepo = () => {
+  console.log('Taro多包复用示例')
+  testHelloMonoRepo()
+  demoApi().then(res => {
 
-  },
+  })
 
-  methods: {
-    testMonoRepo() {
-      console.log('Taro多包复用示例')
-      testHelloMonoRepo()
-      demoApi().then(res => {
-
-      })
-
-      console.log(CommonUtils.randomString(6, 12))
-      console.log(Pipe.hidePart('18863302302', 'phone'))
-      console.log(Constant.PHONE_REGEX)
-      console.log('校验结果: ' + Validate.email('123'))
-
-    }
-
-  }
+  console.log(CommonUtils.randomString(6, 12))
+  console.log(Pipe.hidePart('18863302302', 'phone'))
+  console.log(Constant.PHONE_REGEX)
+  console.log('校验结果: ' + Validate.email('123'))
 
 }
+
 </script>
