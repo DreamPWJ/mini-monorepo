b5a37ba89dd7cf7b28762649b7da67e893865ae6
panweiji <406798106@qq.com>
2024-06-26 15:50:22 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects/pengbo-mini/config/index.ts
+++ projects/pengbo-mini/config/index.ts
@@ -33,13 +33,13 @@ export default defineConfig(async (merge, { command, mode }) => {
     framework: 'vue3',
     compiler: {
       type: 'webpack5',
-      prebundle: { enable: false, force: true }
+      prebundle: { enable: false, force: true } // 开启可用有兼容性问题
     },
     sass: {
       data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
     },
     cache: {
-      enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+      enable: false // Webpack 持久化缓存配置，建议开启。建议开启。开启可用有兼容性问题, 默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
     },
     mini: {
       hot: true,
