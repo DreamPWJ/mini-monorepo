63beb19afd5cd6fe1652019d3ed5cd86143c4093
panweiji <406798106@qq.com>
2024-06-27 10:24:28 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro-mini-vue-demo/components.d.ts
+++ projects-demo/taro-mini-vue-demo/components.d.ts
@@ -11,6 +11,5 @@ declare module 'vue' {
     NutButton: typeof import('@nutui/nutui-taro')['Button']
     NutCalendar: typeof import('@nutui/nutui-taro')['Calendar']
     NutCell: typeof import('@nutui/nutui-taro')['Cell']
-    NutSkeleton: typeof import('@nutui/nutui-taro')['Skeleton']
   }
 }
