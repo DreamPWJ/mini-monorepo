60ae0fd845b391e47db12d9b8150bb6900a971ef
panweiji <406798106@qq.com>
2024-06-29 10:26:51 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro-mini-vue-demo/src/app.ts
+++ projects-demo/taro-mini-vue-demo/src/app.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import 'athena-styles/base.css'
 import 'athena-styles/variable.css'
 import './app.scss'
@@ -9,5 +11,7 @@ const App = createApp({
   }
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
+
+App.use(createPinia())
 
 export default App
