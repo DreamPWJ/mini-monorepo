74afdaa43a9c01616c809a6d4e6ca3072fd53ed0
panweiji <406798106@qq.com>
2024-07-08 15:52:17 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- packages/athena-common/src/index.ts
+++ packages/athena-common/src/index.ts
@@ -1,3 +1,5 @@
 export {Pipe} from './pipe/pipe'
 export {Global} from './common/global'
 export {Validate} from './common/validate'
+
+export { enqueue, clearQueue } from './queue/queue'
--- packages/athena-core/src/index.ts
+++ packages/athena-core/src/index.ts
@@ -1,4 +1,2 @@
 export { Http } from './http/http'
 export type { HttpParams } from './types/http-params'
-
-export { enqueue, clearQueue } from './queue/queue'
