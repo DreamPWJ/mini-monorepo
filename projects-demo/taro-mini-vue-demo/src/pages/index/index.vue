6b04ee845573093f52209336488af5cd7a366c8d
panweiji <406798106@qq.com>
2024-07-11 15:01:36 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro-mini-vue-demo/src/pages/index/index.vue
+++ projects-demo/taro-mini-vue-demo/src/pages/index/index.vue
@@ -66,6 +66,8 @@ const choose = (param) => {
  */
 const testMonoRepo = () => {
   // Taro.navigateTo({ url: '/pages/details/details' })
+
+
   console.log('Taro多包复用示例')
   testHelloMonoRepo()
   testHelloJSMonoRepo()
