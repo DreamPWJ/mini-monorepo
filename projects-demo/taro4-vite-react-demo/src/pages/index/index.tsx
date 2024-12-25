1cb7573662afbd78d53395ce4c7e15add7920978
panweiji <406798106@qq.com>
2024-12-25 14:41:14 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro4-vite-react-demo/src/pages/index/index.tsx
+++ projects-demo/taro4-vite-react-demo/src/pages/index/index.tsx
@@ -1,15 +1,17 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
+import { testHelloMonoRepo } from 'athena-test'
 
 export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
+    testHelloMonoRepo()
   })
 
   return (
     <View className="index">
-      <Text>Hello world!</Text>
+      <Text>Hello World!</Text>
     </View>
   )
 }
