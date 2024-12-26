9f3c37f3bce508cfd0051d47ced9832fa83d53a2
panweiji <406798106@qq.com>
2024-12-26 17:26:16 +0800

    feat: 基于Pnpm管理跨端小程序统一的MonoRepo单体式仓库 单仓多包 Web技术栈

--- projects-demo/taro4-mini-vue-demo/src/pages/index/index.vue
+++ projects-demo/taro4-mini-vue-demo/src/pages/index/index.vue
@@ -4,16 +4,48 @@
   </view>
 </template>
 
-<script>
+<script setup lang="ts">
 import { ref } from 'vue'
 import './index.scss'
+import Taro, { useDidShow, useLoad } from '@tarojs/taro'
+import { testHelloJSMonoRepo, testHelloMonoRepo } from 'athena-test'
+import { demoApi } from '@/api/demo/demo'
+import { CommonUtils } from 'athena-utils'
+import { Pipe, Validate } from 'athena-common'
+import { Constant } from '@athena-constants'
 
-export default {
-  setup() {
-    const msg = ref('Hello world')
-    return {
-      msg
-    }
-  }
+/**
+ * 数据定义
+ */
+const msg = ref('Hello World')
+
+
+useLoad(() => {
+  console.log('Index onLoad')
+})
+
+useDidShow(() =>
+  testMonoRepo()
+)
+
+/**
+ * 测试MonoRepo函数
+ */
+const testMonoRepo = () => {
+  // Taro.navigateTo({ url: '/pages/details/details' })
+
+  console.log('Taro多包复用示例')
+  testHelloMonoRepo()
+  testHelloJSMonoRepo()
+  /*  demoApi().then(res => {
+      apiData.value = res.data
+    })*/
+
+  console.log(CommonUtils.randomString(6, 12))
+  console.log(Pipe.hidePart('18863302302', 'phone'))
+  console.log(Constant.TOKEN_KEY)
+  console.log('校验结果: ' + Validate.email('123'))
+
 }
+
 </script>
